b43aff842a9d07e138d4ada976421c3ea76040a4
lenkan <dlenksjo@gmail.com>
2016-04-03 14:44:08 +0200

    Remove unused PlayerInfo class

---
+++ snakebot-c#/src/Cygni.Snake.SampleBot/MySnakeBot.cs
@@ -0,0 +1,22 @@
+﻿using Cygni.Snake.Client;
+using Cygni.Snake.Client.Communication;
+
+namespace Cygni.Snake.SampleBot
+{
+    public class MySnakeBot : Client.SnakeBot
+    {
+        public MySnakeBot(string name, string color, ISnakeClient snakeClient)
+            : base(name, color, snakeClient)
+        {
+        }
+        
+        protected override Direction OnGameTurn(Map map, long gameTick)
+        {
+            Printer.Enque(map);
+            // figure out a good move
+
+            // do calculated move
+            return Direction.Down;
+        }
+    }
+}
---
+++ snakebot-c#/test/Cygni.Snake.Client.Tests/FakeSnakeBot.cs
@@ -0,0 +1,17 @@
+﻿using Cygni.Snake.Client.Communication;
+
+namespace Cygni.Snake.Client.Tests
+{
+    public class FakeSnakeBot :SnakeBot
+    {
+        public FakeSnakeBot(ISnakeClient client) 
+            : base("fake", "black", client)
+        {
+        }
+
+        protected override Direction OnGameTurn(Map map, long gameTick)
+        {
+            return Direction.Down;
+        }
+    }
+}
