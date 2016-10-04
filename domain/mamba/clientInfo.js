908037d32f263f5e08ac7ee96bff4272682ee9ee
petalv <mail@petteralvsten.com>
2016-10-04 20:35:17 +0200

    issue-3, clientInfo

--- domain/mamba/clientInfo.js
+++ domain/mamba/clientInfo.js
@@ -1,33 +1,21 @@
 function ClientInfo() {
 
-    var type            = 'se.cygni.snake.api.request.ClientInfo';
-    var os              = require('os');
-    var version         = require('../package.json').version || 'unknown';
-    var ifaces          = os.networkInterfaces();
-    var language        = 'JavaScript';
-    var ipAddress       = 'unknown';
-    var operatingSystem = os.type() + ' ' + os.release();
-
-    findIP();
-
-    function findIP() {
-        Object.keys(ifaces).forEach(function (ifname) {
-            var alias = 0;
-            ifaces[ifname].forEach(function (iface) {
-                if ('IPv4' !== iface.family || iface.internal !== false) {
-                    return;
-                }
-                ipAddress = iface.address;
-            });
-        });
-    };
+    var type                   = 'se.cygni.snake.api.request.ClientInfo';
+    var os                     = require('os');
+    var version                = require('../package.json').version || 'unknown';
+    var ifaces                 = os.networkInterfaces();
+    var language               = 'JavaScript';
+    var languageVersion        = process.versions.v8;
+    var operatingSystem        = 'Mac OS X ' + os.type();
+    var operatingSystemVersion = os.release();
 
     var marshall = function () {
         return {
             type: type,
             language: language,
+            languageVersion: languageVersion,
             operatingSystem: operatingSystem,
-            ipAddress: ipAddress,
+            operatingSystemVersion: operatingSystemVersion,
             clientVersion: version
         };
     };
